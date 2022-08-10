7682e2d2e5105f85eacaf19df96966c307be2074
Ivan Zakutniy <ivan.zakutny@flant.com>
2022-08-10 11:14:15 +0300

    bloom filter imp

--- bloom_filter/bloom_filter.go
+++ bloom_filter/bloom_filter.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-        "os"
+        // "os"
 )
 
 type BloomFilter struct {
@@ -30,21 +30,21 @@ func (bf *BloomFilter) Add(s string) {
         hashSum1, hashSum2 := uint32(bf.Hash1(s)), uint32(bf.Hash2(s))
         bitIdx := hashSum1 % uint32(bf.filter_len)
 
-        bf.bitmask |= 1 << uint(bitIdx&0x3f)
+        bf.bitmask |= 1 << uint(bitIdx&0x2C)
         bitIdx = hashSum2 % uint32(bf.filter_len)
-        bf.bitmask |= 1 << uint(bitIdx&0x3f)
+        bf.bitmask |= 1 << uint(bitIdx&0x2C)
 }
 
 func (bf *BloomFilter) IsValue(s string) bool {
         hashSum1, hashSum2 := uint32(bf.Hash1(s)), uint32(bf.Hash2(s))
         bitIdx1, bitIdx2 := hashSum1%uint32(bf.filter_len), hashSum2%uint32(bf.filter_len)
 
-        isValueIdx2 := (bf.bitmask >> uint(bitIdx2&0x3f)) & 1
+        isValueIdx2 := (bf.bitmask >> uint(bitIdx2&0x2C)) & 1
         if isValueIdx2 == 0 {
                 return false
         }
 
-        isValueIdx1 := (bf.bitmask >> uint(bitIdx1&0x3f)) & 1
+        isValueIdx1 := (bf.bitmask >> uint(bitIdx1&0x2C)) & 1
         if isValueIdx1 == 0 {
                 return false
         }
